dfd6ea6d9565bd9477da81fe16e69e2a6ab64a04
anuragchhetri <anurag.chhetri@nagarro.com>
2022-09-13 16:46:53 +0530

    API

--- connection.js
+++ connection.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 AWS.config.update({
   region: "ap-south-1",
   endpoint: "http://localhost:8000",
+  credentials: {
+    accessKeyId: 'fakeMyKeyId',
+    secretAccessKey: 'fakeSecretAccessKey'
+  }
 });
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
